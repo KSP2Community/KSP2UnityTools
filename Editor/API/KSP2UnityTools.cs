a5bd4b9972964e2a4ea15d9b867d3cf6e739edd6
Lexi <arallen3660@gmail.com>
2023-11-06 10:21:09 -0500

    Fix file scoped namespace

--- Editor/API/KSP2UnityTools.cs
+++ Editor/API/KSP2UnityTools.cs
@@ -1,43 +1,44 @@
 ﻿using JetBrains.Annotations;
 
-namespace ksp2community.ksp2unitytools.editor.API;
-
-/// <summary>
-/// This is a global API for interfacing with KSP2 Unity Tools
-/// </summary>
-[PublicAPI]
-public static class KSP2UnityTools
+namespace ksp2community.ksp2unitytools.editor.API
 {
     /// <summary>
-    /// The current mod ID for the project
+    /// This is a global API for interfacing with KSP2 Unity Tools
     /// </summary>
-    public static string CurrentModID => KSP2UnityToolsManager.ProjectModInfo.id;
-    /// <summary>
-    /// The sanitized mod ID for the project
-    /// </summary>
-    public static string CurrentSanitizedModID => KSP2UnityToolsManager.ProjectModInfo.SanitizedId;
-
-    /// <summary>
-    /// Ignore any files that have a certain name when copying files over to the built stuff
-    /// </summary>
-    /// <param name="filename">The s to ignore</param>
-    public static void IgnoreFilesInCopy(params string[] filenames)
+    [PublicAPI]
+    public static class KSP2UnityTools
     {
-        foreach (var filename in filenames)
+        /// <summary>
+        /// The current mod ID for the project
+        /// </summary>
+        public static string CurrentModID => KSP2UnityToolsManager.ProjectModInfo.id;
+        /// <summary>
+        /// The sanitized mod ID for the project
+        /// </summary>
+        public static string CurrentSanitizedModID => KSP2UnityToolsManager.ProjectModInfo.SanitizedId;
+
+        /// <summary>
+        /// Ignore any files that have a certain name when copying files over to the built stuff
+        /// </summary>
+        /// <param name="filename">The s to ignore</param>
+        public static void IgnoreFilesInCopy(params string[] filenames)
         {
-            KSP2UnityToolsManager.Settings.AddIgnoredFile(filename);
+            foreach (var filename in filenames)
+            {
+                KSP2UnityToolsManager.Settings.AddIgnoredFile(filename);
+            }
         }
-    }
 
-    /// <summary>
-    /// Remove any previously ignored filenames from the ignore list
-    /// </summary>
-    /// <param name="filenames">The filenames to remove from the ignore list</param>
-    public static void DontIgnoreFilesInCopy(params string[] filenames)
-    {
-        foreach (var filename in filenames)
+        /// <summary>
+        /// Remove any previously ignored filenames from the ignore list
+        /// </summary>
+        /// <param name="filenames">The filenames to remove from the ignore list</param>
+        public static void DontIgnoreFilesInCopy(params string[] filenames)
         {
-            KSP2UnityToolsManager.Settings.RemoveIgnoredFile(filename);
+            foreach (var filename in filenames)
+            {
+                KSP2UnityToolsManager.Settings.RemoveIgnoredFile(filename);
+            }
         }
     }
 }
