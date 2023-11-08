075c428b86d05696c729c0299602b07e4a6e11d5
Lexi <arallen3660@gmail.com>
2023-11-08 08:15:50 -0500

    Fix addressables API to automatically ensure labels existence

--- Editor/API/AddressablesTools.cs
+++ Editor/API/AddressablesTools.cs
@@ -1,9 +1,11 @@
-﻿using UnityEditor;
+﻿using JetBrains.Annotations;
+using UnityEditor;
 using UnityEditor.AddressableAssets;
 using UnityEditor.AddressableAssets.Settings;
 
 namespace ksp2community.ksp2unitytools.editor.API
 {
+    [PublicAPI]
     public static class AddressablesTools
     {
         public static void MakeAddressable(string assetPath, string name, params string[] labels)
@@ -14,10 +16,17 @@ namespace ksp2community.ksp2unitytools.editor.API
             var entry = settings.CreateOrMoveEntry(guid, group);
             foreach (var label in labels)
             {
+                EnsureLabelIsDefined(settings, label);
                 entry.labels.Add(label);
             }
             entry.address = name;
             settings.SetDirty(AddressableAssetSettings.ModificationEvent.EntryMoved, entry, true);
+        }
+        public static void EnsureLabelIsDefined(AddressableAssetSettings settings, string label) {
+            if (!settings.GetLabels().Contains(label))
+            {
+                settings.AddLabel(label);
+            }
         }
     }
 }
