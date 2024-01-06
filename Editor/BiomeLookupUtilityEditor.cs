dcdd591bd59a31a1712f165fc51c5786ab292eae
Lexi <arallen3660@gmail.com>
2024-01-05 22:24:12 -0500

    Hopefully a working biome map baker

---
+++ Editor/BiomeLookupUtilityEditor.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using KSP.Rendering.Planets;
+using ksp2community.ksp2unitytools.editor.ScriptableObjects;
+using UnityEditor;
+using UnityEngine;
+
+namespace ksp2community.ksp2unitytools.editor
+{
+    [CustomEditor(typeof(BiomeLookupUtility))]
+    public class BiomeLookupUtilityEditor : UnityEditor.Editor
+    {
+        private static PersistentDictionary _hashMapNames;
+        private static PersistentDictionary _lutNames;
+
+        private static PersistentDictionary HashMapNames =>
+            _hashMapNames ??= KSP2UnityToolsManager.GetDictionary("HashMapNames");
+
+        private static PersistentDictionary LutNames => _lutNames ??= KSP2UnityToolsManager.GetDictionary("LUTNames");
+        private BiomeLookupUtility Target => target as BiomeLookupUtility;
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            GUILayout.Label("Hash Map Saving", EditorStyles.boldLabel);
+            var hashMapName = HashMapNames.TryGetValue(Target.name, out var newHashMapName)
+                ? newHashMapName
+                : $"{Target.name} Hashmap";
+            var lutName = LutNames.TryGetValue(Target.name, out var newLutName) ? newLutName : $"{Target.name} LUT";
+            if (GUILayout.Button("Bake"))
+            {
+                var path = AssetDatabase.GetAssetPath(Target);
+                if (path == "")
+                {
+                    path = "Assets";
+                }
+                else if (Path.GetExtension(path) != "")
+                {
+                    path = path.Replace(Path.GetFileName (AssetDatabase.GetAssetPath (Selection.activeObject)), "");
+                }
+                
+                var lut = CreateInstance<BiomeTextureColorLookupTable>();
+                var hashmap = CreateInstance<BiomeLookupHashTable>();
+                
+                for (var i = 0; i < Target.colorMapping.Count; i++)
+                {
+                    EditorUtility.DisplayProgressBar("Baking Biome Map", $"Building Color LUT {i+1}/{Target.colorMapping.Count}", (i + 1)/(float)Target.colorMapping.Count);
+                    lut.BiomeLookupPairs.Add(new BiomeLookupEditorPair
+                    {
+                        name = Target.colorMapping[i].name,
+                        color = Target.colorMapping[i].color,
+                    });
+                }
+                AssetDatabase.CreateAsset(lut, AssetDatabase.GenerateUniqueAssetPath(path + $"/{lutName}.asset"));
+                for (var y = 0; y < 256; y++)
+                {
+                    var baseIndex = y * 256;
+                    
+                    for (var x = 0; x < 256; x++)
+                    {
+                        var index = y * 256;
+                        EditorUtility.DisplayProgressBar("Baking Biome Map",
+                            $"Building Hashmap (x = {x:03}/255, y = {y:03}/255)", index / 65535.0f);
+                    }
+                }
+                
+                AssetDatabase.CreateAsset(hashmap,
+                    AssetDatabase.GenerateUniqueAssetPath(path + $"/{hashMapName}.asset"));
+                AssetDatabase.Refresh();
+                EditorUtility.ClearProgressBar();
+            }
+        }
+    }
+}
---
+++ Editor/ScriptableObjects/BiomeLookupUtility.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KSP.Rendering.Planets;
+using UnityEngine;
+
+namespace ksp2community.ksp2unitytools.editor.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "BiomeLookup",menuName = "KSP2UT/Biome Lookup Utility")]
+    public class BiomeLookupUtility : ScriptableObject
+    {
+        public Texture2D biomeMap;
+
+        public List<ColorInfo> colorMapping = new();
+
+
+        private Color GetScaled(int scaledX, int scaledY)
+        {
+            var actualX = (int)((long)scaledX * biomeMap.width / 4096L);
+            var actualY = (int)((long)scaledY * biomeMap.height / 4096L);
+            return biomeMap.GetPixel(actualX, actualY);
+        }
+        
+        private Color[,] GetColors(int cellX, int cellY)
+        {
+            var result = new Color[16, 16];
+            var x16 = cellX * 16;
+            var y16 = cellY * 16;
+            if (biomeMap == null) return result;
+            for (var y = 0; y < 16; y++)
+            {
+                var scaledY = y16 * 16 + y;
+                for (var x = 0; x < 16; x++)
+                {
+                    result[y, x] = GetScaled(x16 + x, y16);
+                }
+            }
+            return result;
+        }
+
+        private int ConvertToIndex(Color col)
+        {
+            var bestIndex = 0;
+            var closestDistance = float.MaxValue;
+            for (var i = 0; i < colorMapping.Count; i++)
+            {
+                var indexColor = colorMapping[i].color;
+                var distanceSquared = (col.r - indexColor.r) * (col.r - indexColor.r) +
+                                      (col.g - indexColor.g) * (col.g - indexColor.g) +
+                                      (col.b - indexColor.b) * (col.b - indexColor.b) +
+                                      (col.a - indexColor.a) * (col.a - indexColor.a);
+                if (!(distanceSquared < closestDistance)) continue;
+                closestDistance = distanceSquared;
+                bestIndex = i;
+            }
+            return bestIndex;
+        }
+
+        private int[,] ConvertToIndices(Color[,] cell)
+        {
+            var result = new int[16, 16];
+            for (var y = 0; y < 16; y++)
+            {
+                for (var x = 0; x < 16; x++)
+                {
+                    result[y, x] = ConvertToIndex(cell[y, x]);
+                }
+            }
+            return result;
+        }
+
+        private static IEnumerable<RectangleInfo> Rectangles(int[,] indices)
+        {
+            var result = new List<RectangleInfo>();
+            while (true)
+            {
+                int startX, startY, index;
+                for (startY = 0; startY < 16; startY++)
+                {
+                    for (startX = 0; startX < 16; startX++)
+                    {
+                        index = indices[startY, startX];
+                        if (!AlreadyMarked(startX, startY)) goto found_start;
+                    }
+                }
+                break;
+                found_start:
+                int endX = startX, endY = startY;
+                for (; endX < 16; endX++)
+                {
+                    if (indices[startY, startX] == index) continue;
+                    endX -= 1;
+                    break;
+                }
+
+                for (; endY < 16; endY++)
+                {
+                    var foundEnd = false;
+                    for (var x = startX; x <= endX; x++)
+                    {
+                        if (indices[endY, x] == index) continue;
+                        foundEnd = true;
+                        break;
+                    }
+                    if (!foundEnd) continue;
+                    endY -= 1;
+                    break;
+                }
+
+                result.Add(new RectangleInfo
+                {
+                    minX = startX, 
+                    minY = startY, 
+                    maxX = endX, 
+                    maxY = endY, 
+                    index = index
+                });
+            }
+
+            return result;
+
+            bool AlreadyMarked(int x, int y)
+            {
+                return result.Any(rectangle => x >= rectangle.minX && x <= rectangle.maxX && y >= rectangle.minY && y <= rectangle.maxY);
+            }
+        }
+
+        public List<int> GetCell(int x, int y) => Rectangles(ConvertToIndices(GetColors(x, y)))
+            .Select(rectangleInfo => rectangleInfo.ToPackedData(this)).ToList();
+
+
+        private class RectangleInfo
+        {
+            internal int minX;
+            internal int maxX;
+            internal int minY;
+            internal int maxY;
+            internal int index;
+
+            internal int ToPackedData(BiomeLookupUtility utility)
+            {
+                var info = utility.colorMapping[index];
+                var type = info.type;
+                return BiomeLookupHashTable.PackBiomeChunkData(minX, maxX, minY, maxY, (int)type, index);
+            }
+        }
+        
+        [Serializable]
+        public class ColorInfo
+        {
+            public string name;
+            public Color color;
+            public PQSData.KSP2BiomeType type;
+        }
+    }
+}
