13209b932203ef4df861ea5eb34f1c97cf48dc12
Lexi <arallen3660@gmail.com>
2024-01-05 22:44:15 -0500

    Clear progress bar on error

--- Editor/BiomeLookupUtilityEditor.cs
+++ Editor/BiomeLookupUtilityEditor.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using KSP.Rendering.Planets;
 using ksp2community.ksp2unitytools.editor.ScriptableObjects;
@@ -30,49 +31,63 @@ namespace ksp2community.ksp2unitytools.editor
             LutNames[Target.name] = lutName = EditorGUILayout.TextField("LUT Name", lutName);
             if (GUILayout.Button("Bake"))
             {
-                var path = AssetDatabase.GetAssetPath(Target);
-                if (path == "")
+                try
                 {
-                    path = "Assets";
-                }
-                else if (Path.GetExtension(path) != "")
-                {
-                    path = path.Replace(Path.GetFileName (AssetDatabase.GetAssetPath (Selection.activeObject)), "");
-                }
-                
-                var lut = CreateInstance<BiomeTextureColorLookupTable>();
-                lut.BiomeLookupPairs = new List<BiomeLookupEditorPair>();
-                var hashmap = CreateInstance<BiomeLookupHashTable>();
-                
-                for (var i = 0; i < Target.colorMapping.Count; i++)
-                {
-                    EditorUtility.DisplayProgressBar("Baking Biome Map", $"Building Color LUT {i+1}/{Target.colorMapping.Count}", (i + 1)/(float)Target.colorMapping.Count);
-                    lut.BiomeLookupPairs.Add(new BiomeLookupEditorPair
+                    var path = AssetDatabase.GetAssetPath(Target);
+                    if (path == "")
                     {
-                        name = Target.colorMapping[i].name,
-                        color = Target.colorMapping[i].color,
-                    });
-                }
-                AssetDatabase.CreateAsset(lut, AssetDatabase.GenerateUniqueAssetPath(path + $"/{lutName}.asset"));
-                for (var y = 0; y < 256; y++)
-                {
-                    var baseIndex = y * 256;
-                    
-                    for (var x = 0; x < 256; x++)
-                    {
-                        var index = y * 256;
-                        EditorUtility.DisplayProgressBar("Baking Biome Map",
-                            $"Building Hashmap (x = {x:03}/255, y = {y:03}/255)", index / 65535.0f);
-                        hashmap.Cells[index] = new BiomeLookupHashCell
-                        {
-                            BiomeChunks = Target.GetCell(x,y)
-                        };
+                        path = "Assets";
                     }
+                    else if (Path.GetExtension(path) != "")
+                    {
+                        path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+                    }
+
+                    var lut = CreateInstance<BiomeTextureColorLookupTable>();
+                    lut.BiomeLookupPairs = new List<BiomeLookupEditorPair>();
+                    var hashmap = CreateInstance<BiomeLookupHashTable>();
+
+                    for (var i = 0; i < Target.colorMapping.Count; i++)
+                    {
+                        EditorUtility.DisplayProgressBar("Baking Biome Map",
+                            $"Building Color LUT {i + 1}/{Target.colorMapping.Count}",
+                            (i + 1) / (float)Target.colorMapping.Count);
+                        lut.BiomeLookupPairs.Add(new BiomeLookupEditorPair
+                        {
+                            name = Target.colorMapping[i].name,
+                            color = Target.colorMapping[i].color,
+                        });
+                    }
+
+                    AssetDatabase.CreateAsset(lut, AssetDatabase.GenerateUniqueAssetPath(path + $"/{lutName}.asset"));
+                    for (var y = 0; y < 256; y++)
+                    {
+                        var baseIndex = y * 256;
+
+                        for (var x = 0; x < 256; x++)
+                        {
+                            var index = y * 256;
+                            EditorUtility.DisplayProgressBar("Baking Biome Map",
+                                $"Building Hashmap (x = {x:03}/255, y = {y:03}/255)", index / 65535.0f);
+                            hashmap.Cells[index] = new BiomeLookupHashCell
+                            {
+                                BiomeChunks = Target.GetCell(x, y)
+                            };
+                        }
+                    }
+
+                    AssetDatabase.CreateAsset(hashmap,
+                        AssetDatabase.GenerateUniqueAssetPath(path + $"/{hashMapName}.asset"));
                 }
-                EditorUtility.ClearProgressBar();
-                AssetDatabase.CreateAsset(hashmap,
-                    AssetDatabase.GenerateUniqueAssetPath(path + $"/{hashMapName}.asset"));
-                AssetDatabase.Refresh();
+                catch (Exception e)
+                {
+                    Debug.LogError(e);
+                }
+                finally
+                {
+                    EditorUtility.ClearProgressBar();
+                    AssetDatabase.Refresh();
+                }
             }
         }
     }
