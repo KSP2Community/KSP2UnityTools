30e63b749a9436ddadae3ae840e465d9a107baff
Lexi <arallen3660@gmail.com>
2024-01-16 13:01:30 -0500

    Lets hope this works

---
+++ Editor/CustomEditors/PqsDecalUtilityEditor.cs
@@ -0,0 +1,60 @@
+﻿using System.IO;
+using ksp2community.ksp2unitytools.editor.ScriptableObjects;
+using UniLinq;
+using UnityEditor;
+using UnityEngine;
+
+namespace ksp2community.ksp2unitytools.editor.CustomEditors
+{
+    [CustomEditor(typeof(PqsDecalUtility))]
+    public class PqsDecalUtilityEditor : UnityEditor.Editor
+    {
+        private static PersistentDictionary _pqsDecalDataNames;
+
+        private static PersistentDictionary PqsDecalNames =>
+            _pqsDecalDataNames ??= KSP2UnityToolsManager.GetDictionary("PqsDecalNames");
+        
+        private PqsDecalUtility Target => target as PqsDecalUtility;
+
+        private static (ushort[] data, int width, int height) GetDataFrom(Texture2D texture16)
+        {
+            var rawData = texture16.GetRawTextureData<ushort>();
+            var data = new ushort[texture16.width * texture16.height];
+            rawData.CopyTo(data);
+            return (data, texture16.width, texture16.height);
+        }
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            GUILayout.Label("PQS Decal Data Baking", EditorStyles.boldLabel);
+            var dataName = PqsDecalNames.TryGetValue(Target.name, out var newDataName) ? newDataName : "NewPQSData";
+            if (GUILayout.Button("Bake"))
+            {
+                var decalData = CreateInstance<PQSDecalData>();
+                decalData.BakedPqsDecalList = Target.decals;
+                decalData.BakedPqsDecalIDList = Target.decals.Select(x => x.DecalID).ToList();
+                decalData.DiffuseTextureArray = Target.diffuse;
+                decalData.NormalTextureArray = Target.normal;
+                decalData.AlphaMaskTextureArray = Target.alphaMask;
+                decalData.PeakTextureArray = Target.peak;
+                decalData.SlopeTextureArray = Target.slope;
+                decalData.Count = Target.decals.Count;
+                (decalData.HeightData, decalData.HeightWidth, decalData.HeightHeight) = GetDataFrom(Target.heightData);
+                (decalData.AlphaData, decalData.AlphaWidth, decalData.AlphaHeight) = GetDataFrom(Target.alphaData);
+                var path = AssetDatabase.GetAssetPath(Target);
+                if (path == "")
+                {
+                    path = "Assets";
+                }
+                else if (Path.GetExtension(path) != "")
+                {
+                    path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+                }
+
+                if (File.Exists($"{path}/{dataName}.asset")) AssetDatabase.DeleteAsset($"{path}/{dataName}.asset");
+                AssetDatabase.CreateAsset(decalData,$"{path}/{dataName}.asset");
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+}
---
+++ Editor/ScriptableObjects/PqsDecalUtility.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace ksp2community.ksp2unitytools.editor.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "PqsDecalUtility",menuName = "KSP2UT/Pqs Decal Utility")]
+    public class PqsDecalUtility : ScriptableObject
+    {
+
+        [Header("Decals")] 
+        [Tooltip("A list of decal instances")]
+        public List<PQSDecal> decals;
+
+        [Header("Texture Arrays")] 
+        [Tooltip("The diffuse textures for decals")]
+        public Texture2DArray diffuse;
+        [Tooltip("The normal textures for decals")]
+        public Texture2DArray normal;
+        [Tooltip("The alpha mask textures for decals")]
+        public Texture2DArray alphaMask;
+        [Tooltip("The peak textures for decals")]
+        public Texture2DArray peak;
+        [Tooltip("The slope textures for decals")]
+        public Texture2DArray slope;
+        
+        [Header("Maps")]
+        [Tooltip("A R16 texture containing the height data")]
+        public Texture2D heightData;
+        [Tooltip("A R16 texture containing the alpha data")]
+        public Texture2D alphaData;
+        
+        
+    }
+}
